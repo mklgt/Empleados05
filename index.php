ab7c0b2b9bb823506138d7539f1c40c7034eab43
Mikel Goicoechea - DAW 2 <56249182+mklgt@users.noreply.github.com>
2021-11-17 10:19:57 +0100

    Cambiado los números de la seguridad social
    Cambiado los números de la seguridad social al formato de España.

--- index.php
+++ index.php
@@ -10,15 +10,15 @@ use Empresaempleado\PruebaPolimorf as Polimorfismo;
 $empresa = new Compañia();
 $resultado = "";
 
-$emp1 = new Plantilla("Asier", "Gonzalez", "319-20-7218", 800, 75);
-$emp2 = new Plantilla("Mikel", "Goicoechea", "434-22-8641", 1000, 100);
+$emp1 = new Plantilla("Asier", "Gonzalez", "01-36500028-76", 800, 75);
+$emp2 = new Plantilla("Mikel", "Goicoechea", "32-90891510-58", 1000, 100);
 $resultado .= "<h2>Empleado:</h2>";
 $resultado .= "<h2>Empleado Asalariado:</h2>";
 $resultado .= $emp1->mostrar();
 $resultado .= "<br>" . $emp2->mostrar() . "<br>";
 
-$empc1 = new EmpleadoComision("Iñigo", "Aguirre", "319-20-7218", 30, 350, 175);
-$empc2 = new EmpleadoComision("Aimar", "Goicoechea", "434-22-8641", 35, 200, 325);
+$empc1 = new EmpleadoComision("Iñigo", "Aguirre", "40-97211580-62", 30, 350, 175);
+$empc2 = new EmpleadoComision("Aimar", "Goicoechea", "29-30586572-68", 35, 200, 325);
 $resultado .= "<h2>Empleado por comisión:</h2>";
 $resultado .= $empc1->mostrar();
 $resultado .= "<br>" . $empc2->mostrar() . "<br>";
